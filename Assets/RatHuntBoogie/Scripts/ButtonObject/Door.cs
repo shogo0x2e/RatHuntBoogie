88d107e998710b18d33eedf37f0e15c86f4cd02c
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-11-19 14:29:05 +0100

    Can attach objects to button

---
+++ Assets/RatHuntBoogie/Scripts/ButtonObject/Door.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Door : ButtonObject {
+    public override void OnButtonPressed() {
+        Debug.Log("Hello");
+    }
+
+    public override void OnButtonReleased() { }
+}
