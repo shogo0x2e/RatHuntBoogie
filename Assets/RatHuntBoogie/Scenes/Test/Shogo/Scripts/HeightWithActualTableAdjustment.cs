b1d797693f0d6d23250ca2498ee819f0657377a1
Shogo Kitada <contact@shogo0x2e.com>
2024-12-10 19:30:15 +0100

    implemented height adjusting

---
+++ Assets/RatHuntBoogie/Scenes/Test/Shogo/Scripts/HeightWithActualTableAdjustment.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HeightWithActualTableAdjustment : MonoBehaviour
+{
+    [SerializeField] 
+    private float _tableHeight = 0.74f;
+    
+    [SerializeField] 
+    private CapsuleCollider _touchingCollider;
+
+    [SerializeField] 
+    private Transform _playerRigVR;
+
+    [SerializeField] private TextMeshProUGUI _debugText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var colliderOffset = _touchingCollider.radius / 2;
+        var headHeightFromActualFloor = _playerRigVR.InverseTransformPoint(transform.position).y;
+        var colliderCenter = _touchingCollider.center;
+        
+        _touchingCollider.center = new Vector3(
+            colliderCenter.x,
+            -(headHeightFromActualFloor - _tableHeight - colliderOffset),
+            colliderCenter.z
+        );
+        _debugText.text = $"{headHeightFromActualFloor - _tableHeight}";
+    }
+}
